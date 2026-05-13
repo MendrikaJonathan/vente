ffa765db23bd9c6c83ae8c884746856e6d1db544
jonathan <jonathanrazafy487@gmail.com>
2026-05-13 09:02:27 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl  = import."https://bmmbqxafepnixnnajonm.supabase.co"
-const supabaseKey  = import."sb_publishable_RTNMqL0_zKAb_xyV7l5f4Q__cyLO8yr"
+//const supabaseUrl  = import."https://bmmbqxafepnixnnajonm.supabase.co"
+//const supabaseKey  = import."sb_publishable_RTNMqL0_zKAb_xyV7l5f4Q__cyLO8yr"
 
 if (!supabaseUrl || !supabaseKey) {
   console.warn(
@@ -11,8 +11,8 @@ if (!supabaseUrl || !supabaseKey) {
 }
 
 export const supabase = createClient(
-  supabaseUrl  || '"https://bmmbqxafepnixnnajonm.supabase.co',
-  supabaseKey  || 'sb_publishable_RTNMqL0_zKAb_xyV7l5f4Q__cyLO8yr',
+  '"https://bmmbqxafepnixnnajonm.supabase.co',
+   'sb_publishable_RTNMqL0_zKAb_xyV7l5f4Q__cyLO8yr',
   {
     auth: {
       persistSession: true,
