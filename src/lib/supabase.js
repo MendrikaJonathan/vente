fd4e03227dc75ed71dda91b732bf72b13bf484cb
jonathan <jonathanrazafy487@gmail.com>
2026-05-13 09:04:14 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,12 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 //const supabaseUrl  = import."https://bmmbqxafepnixnnajonm.supabase.co"
 //const supabaseKey  = import."sb_publishable_RTNMqL0_zKAb_xyV7l5f4Q__cyLO8yr"
 
-if (!supabaseUrl || !supabaseKey) {
+/*if (!supabaseUrl || !supabaseKey) {
   console.warn(
     '⚠️  Variables Supabase manquantes.\n' +
     'Copiez .env.example en .env et remplissez VITE_SUPABASE_URL et VITE_SUPABASE_ANON_KEY.'
   )
-}
+}*/
 
 export const supabase = createClient(
   '"https://bmmbqxafepnixnnajonm.supabase.co',
